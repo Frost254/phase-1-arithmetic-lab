e6e03bbf8d1b7fda3d78a0b4b889ec4ecf709ce5
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-16 23:10:47 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 8;
+let num5 = 20;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(num2, num3, num4, num5);
